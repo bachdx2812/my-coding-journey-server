0d9a6c114743fbba0fa683e2401ec590aeaefd9d
bachdx2812 <bachdx.hut@gmail.com>
2024-01-03 12:02:14 +0700

    add test route

--- api/index.js
+++ api/index.js
@@ -7,6 +7,11 @@ const routers = require("./routes");
 app.use(bodyParser.json());
 
 app.use("/change_logs", routers.changeLogsRouters);
+
+app.get("/api/test", (req, res) => {
+  res.end("jkljklqw");
+});
+
 const port = process.env.PORT || "3000";
 
 app.listen(port);
