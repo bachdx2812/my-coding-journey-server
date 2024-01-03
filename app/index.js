d45edb85ea5ae60106536b61f4c0806d86b17613
bachdx2812 <bachdx.hut@gmail.com>
2024-01-03 14:52:03 +0700

    add cors and api rate limit

--- app/index.js
+++ app/index.js
@@ -2,11 +2,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const { rateLimit } = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+});
+
 const app = express();
 const routers = require("./routes");
 
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://mycodejourney.vercel.app/", "http://localhost:5173"],
+  })
+);
 
 app.use("/change_logs", routers.changeLogsRouters);
 
